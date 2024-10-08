102bb765fea6400658b6180d0b43bf7a21154c0f
saurav11sarkar <sarkarsaurav11@outlook.com>
2024-10-08 15:05:29 +0600

    react senipad

---
+++ modual_57/my-vue-app/src/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti laboriosam a ratione in sed doloribus voluptate earum cupiditate sunt voluptas. Commodi, debitis tempore blanditiis aspernatur omnis dolorum quis suscipit repellat.</p>
+        </div>
+    );
+};
+
+export default Home;
